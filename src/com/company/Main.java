c93fcad9c9b6e01775039b55b88c6aeb1bc9b85b
Hokimko <abubekerovkhakim@gmail.com>
2021-06-12 14:36:50 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello, World!");
+
+        float a = 50.5f;
+        float b = 10.1f;
+        double c = a / b;
+
+        System.out.println("result: " + c);
+
+        String name = "khakim";
+        String surname = "Abubekerov";
+        System.out.println(name + " " + surname);
+
+        final String s = "Hi";
+
+        System.out.println(s + ", " + name + " " + surname );
+
+
+
+    }
+
+}
